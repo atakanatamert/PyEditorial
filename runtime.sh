31b1300151ce632a8262a32518052a538be51083
atakanatamert <atknatmrt@gmail.com>
2023-10-02 12:56:58 +0300

    Add runtime script

---
+++ runtime.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python manage.py makemigrations
+python manage.py migrate
+python manage.py migrate --run-syncdb
+python manage.py collectstatic --noinput
+
+gunicorn -b 0.0.0.0:8000 PyEditorial.wsgi:application
